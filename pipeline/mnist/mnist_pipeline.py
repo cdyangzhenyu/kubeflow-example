3820eaa2927da81817d782ec70cdf84d1f8485bb
kube <kube@test.example>
2019-06-13 15:29:08 +0800

    add mnist pipeline demo

--- pipeline/mnist/mnist_pipeline.py
+++ pipeline/mnist/mnist_pipeline.py
@@ -28,29 +28,48 @@ platform = 'onprem'
   name='MNIST',
   description='A pipeline to train and serve the MNIST example.'
 )
-def mnist_pipeline(model_export_dir='gs://your-bucket/export',
-                   train_steps='200',
+def mnist_pipeline(model_export_dir='/mnt/export',
+                   train_steps='1000',
                    learning_rate='0.01',
                    batch_size='100',
-                   pvc_name=''):
+                   output='/mnt'):
   """
   Pipeline with three stages:
     1. train an MNIST classifier
     2. deploy a tf-serving instance to the cluster
     3. deploy a web-ui to interact with it
   """
+
+  if platform != 'GCP':
+        vop = dsl.VolumeOp(
+            name="create_pvc",
+            storage_class="rook-ceph-fs",
+            resource_name="pipeline-pvc",
+            modes=dsl.VOLUME_MODE_RWM,
+            size="1Gi"
+        )
+        pvc_name = vop.outputs["name"]
+
+        download = dsl.ContainerOp(
+            name="download_data",
+            image="alpine/git",
+            command=["git", "clone", "https://github.com/cdyangzhenyu/mnist-data.git", str(output) + "/data"],
+        ).apply(onprem.mount_pvc(pvc_name, 'local-storage', output))
+        download.after(vop)
+
   train = dsl.ContainerOp(
       name='train',
       image='gcr.io/kubeflow-examples/mnist/model:v20190304-v0.2-176-g15d997b',
       arguments=[
           "/opt/model.py",
+          "--tf-data-dir", str(output) + "/data",
           "--tf-export-dir", model_export_dir,
           "--tf-train-steps", train_steps,
           "--tf-batch-size", batch_size,
           "--tf-learning-rate", learning_rate
           ]
   )
-
+  train.after(download)
 
   serve_args = [
       '--model-export-path', model_export_dir,
@@ -76,8 +95,8 @@ def mnist_pipeline(model_export_dir='gs://your-bucket/export',
                      'v20190304-v0.2-176-g15d997b-pipelines',
           '--name', 'web-ui',
           '--container-port', '5000',
-          '--service-port', '80',
-          '--service-type', "LoadBalancer"
+          '--service-port', '30080',
+          '--service-type', "NodePort"
   ]
   if platform != 'GCP':
     webui_args.extend([
@@ -88,7 +107,7 @@ def mnist_pipeline(model_export_dir='gs://your-bucket/export',
       name='web-ui',
       image='gcr.io/kubeflow-examples/mnist/deploy-service:latest',
       arguments=webui_args
-  )
+  ).set_image_pull_policy('IfNotPresent')
   web_ui.after(serve)
 
   steps = [train, serve, web_ui]
@@ -96,8 +115,9 @@ def mnist_pipeline(model_export_dir='gs://your-bucket/export',
     if platform == 'GCP':
       step.apply(gcp.use_gcp_secret('user-gcp-sa'))
     else:
-      step.apply(onprem.mount_pvc(pvc_name, 'local-storage', '/mnt'))
+      step.apply(onprem.mount_pvc(pvc_name, 'local-storage', output))
 
 if __name__ == '__main__':
   import kfp.compiler as compiler
   compiler.Compiler().compile(mnist_pipeline, __file__ + '.tar.gz')
+
