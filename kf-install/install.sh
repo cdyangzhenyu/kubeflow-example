6042f300494054679909b364b22cd031fcf8905b
cdyangzhenyu <cdyangzhenyu@gmail.com>
2019-06-27 16:00:21 +0800

    modify katib from v2 to v1

--- kf-install/install.sh
+++ kf-install/install.sh
@@ -7,12 +7,19 @@ kubectl create namespace kubeflow
 ./kustomize build jupyter/jupyter-web-app/base/ | kubectl apply -f -
 ./kustomize build jupyter/notebook-controller/base/ | kubectl apply -f -
 
-./kustomize build katib-v1alpha2/katib-controller/base/ | kubectl apply -f -
-./kustomize build katib-v1alpha2/katib-db/base/ | kubectl apply -f -
-./kustomize build katib-v1alpha2/katib-manager/base/ | kubectl apply -f -
-./kustomize build katib-v1alpha2/katib-ui/base/ | kubectl apply -f -
-./kustomize build katib-v1alpha2/metrics-collector/base/ | kubectl apply -f -
-./kustomize build katib-v1alpha2/suggestion/base/ | kubectl apply -f -
+#./kustomize build katib-v1alpha2/katib-controller/base/ | kubectl apply -f -
+#./kustomize build katib-v1alpha2/katib-db/base/ | kubectl apply -f -
+#./kustomize build katib-v1alpha2/katib-manager/base/ | kubectl apply -f -
+#./kustomize build katib-v1alpha2/katib-ui/base/ | kubectl apply -f -
+#./kustomize build katib-v1alpha2/metrics-collector/base/ | kubectl apply -f -
+#./kustomize build katib-v1alpha2/suggestion/base/ | kubectl apply -f -
+
+./kustomize build katib-v1alpha1/vizier-core/base/ | kubectl apply -f -
+./kustomize build katib-v1alpha1/vizier-db/base/ | kubectl apply -f -
+./kustomize build katib-v1alpha1/studyjob/base/ | kubectl apply -f -
+./kustomize build katib-v1alpha1/katib-ui/base/ | kubectl apply -f -
+./kustomize build katib-v1alpha1/metrics-collector/base/ | kubectl apply -f -
+./kustomize build katib-v1alpha1/suggestion/base/ | kubectl apply -f -
 
 ./kustomize build pipeline/mysql/base/ | kubectl apply -f -
 ./kustomize build pipeline/minio/base/ | kubectl apply -f -
