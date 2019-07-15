b6aec42abb16b88bfb9df6752257ec1992a0d139
cdyangzhenyu <cdyangzhenyu@gmail.com>
2019-07-15 10:00:06 +0800

    update

--- kf-install/install.sh
+++ kf-install/install.sh
@@ -1,14 +1,21 @@
 kubectl create namespace kubeflow
 
+#main
 ./kustomize build metacontroller/base/ | kubectl apply -f -
 ./kustomize build argo/base/ | kubectl apply -f -
 ./kustomize build common/centraldashboard/base/ | kubectl apply -f -
 ./kustomize build common/ambassador/base/ | kubectl apply -f -
+./kustomize build tensorboard/base/ | kubectl apply -f -
+./kustomize build tf-training/tf-job-operator/base/ | kubectl apply -f -
+./kustomize build pytorch-job/pytorch-job-crds/base/ | kubectl apply -f -
+./kustomize build pytorch-job/pytorch-operator/base/ | kubectl apply -f -
 
+#jupyter
 ./kustomize build jupyter/jupyter/base/ | kubectl apply -f -
 ./kustomize build jupyter/jupyter-web-app/base/ | kubectl apply -f -
 ./kustomize build jupyter/notebook-controller/base/ | kubectl apply -f -
 
+#katib-v1alpha2
 #./kustomize build katib-v1alpha2/katib-controller/base/ | kubectl apply -f -
 #./kustomize build katib-v1alpha2/katib-db/base/ | kubectl apply -f -
 #./kustomize build katib-v1alpha2/katib-manager/base/ | kubectl apply -f -
@@ -16,13 +23,16 @@ kubectl create namespace kubeflow
 #./kustomize build katib-v1alpha2/metrics-collector/base/ | kubectl apply -f -
 #./kustomize build katib-v1alpha2/suggestion/base/ | kubectl apply -f -
 
-./kustomize build katib-v1alpha1/vizier-core/base/ | kubectl apply -f -
-./kustomize build katib-v1alpha1/vizier-db/base/ | kubectl apply -f -
-./kustomize build katib-v1alpha1/studyjob/base/ | kubectl apply -f -
-./kustomize build katib-v1alpha1/katib-ui/base/ | kubectl apply -f -
-./kustomize build katib-v1alpha1/metrics-collector/base/ | kubectl apply -f -
-./kustomize build katib-v1alpha1/suggestion/base/ | kubectl apply -f -
+#katib-v1alpha1
+#./kustomize build katib-v1alpha1/vizier-core/base/ | kubectl apply -f -
+#./kustomize build katib-v1alpha1/vizier-db/base/ | kubectl apply -f -
+#./kustomize build katib-v1alpha1/studyjob/base/ | kubectl apply -f -
+#./kustomize build katib-v1alpha1/katib-ui/base/ | kubectl apply -f -
+#./kustomize build katib-v1alpha1/metrics-collector/base/ | kubectl apply -f -
+#./kustomize build katib-v1alpha1/suggestion/base/ | kubectl apply -f -
+#./kustomize build modeldb/base/ | kubectl apply -f -
 
+#kfp
 ./kustomize build pipeline/mysql/base/ | kubectl apply -f -
 ./kustomize build pipeline/minio/base/ | kubectl apply -f -
 ./kustomize build pipeline/pipelines-runner/base/ | kubectl apply -f -
@@ -32,8 +42,3 @@ kubectl create namespace kubeflow
 ./kustomize build pipeline/scheduledworkflow/base/ | kubectl apply -f -
 ./kustomize build pipeline/api-service/base/ | kubectl apply -f -
 
-./kustomize build tensorboard/base/ | kubectl apply -f -
-
-./kustomize build tf-training/tf-job-operator/base/ | kubectl apply -f -
-./kustomize build pytorch-job/pytorch-job-crds/base/ | kubectl apply -f -
-./kustomize build pytorch-job/pytorch-operator/base/ | kubectl apply -f -
